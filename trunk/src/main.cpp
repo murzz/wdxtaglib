4940364ba5ab978ac1fb820ff8ca5fd03a103d16
murzz <murzz@a772d836-fa84-11dd-8259-6defae454439>
2008-03-01 20:46:43 +0000

    Initial import for WDXTagLib project. This is the content plugin for Total Commander. It uses TagLib as an engine and gives access to audio files tags.

---
+++ trunk/src/main.cpp
@@ -0,0 +1,89 @@
+#include "main.h"
+#include <fileref.h>
+#include <tag.h>
+#include <map>
+
+#define _detectstring ""
+
+// a sample exported function
+/*void DLL_EXPORT SomeFunction(const LPCSTR sometext)
+{
+    MessageBoxA(0, sometext, "DLL Message", MB_OK | MB_ICONINFORMATION);
+}*/
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+    switch (fdwReason)
+    {
+        case DLL_PROCESS_ATTACH:
+            // attach to process
+            // return FALSE to fail DLL load
+            break;
+
+        case DLL_PROCESS_DETACH:
+            // detach from process
+            break;
+
+        case DLL_THREAD_ATTACH:
+            // attach to thread
+            break;
+
+        case DLL_THREAD_DETACH:
+            // detach from thread
+            break;
+    }
+    return TRUE; // succesful
+}
+
+char* strlcpy(char* p,const char* p2,int maxlen)
+{
+	if ((int)strlen(p2)>=maxlen) {
+			strncpy(p,p2,maxlen);
+			p[maxlen]=0;
+	} else
+			strcpy(p,p2);
+	return p;
+}
+
+TagLib::String formatSeconds(int seconds)
+{
+  char secondsString[3];
+  sprintf(secondsString, "%02i", seconds);
+  return secondsString;
+}
+
+int DLL_EXPORT __stdcall ContentGetDetectString(char* DetectString,int maxlen)
+{
+	strlcpy(DetectString,_detectstring,maxlen);
+	return 0;
+}
+
+void DLL_EXPORT __stdcall ContentSetDefaultParams(ContentDefaultParamStruct* dps)
+{
+	if ( sizeof(ContentDefaultParamStruct) != dps->size)
+		return;
+	MessageBoxA(0, dps->DefaultIniName, "DLL Message", MB_OK | MB_ICONINFORMATION);
+}
+
+int DLL_EXPORT __stdcall ContentGetSupportedField(int FieldIndex,char* FieldName,char* Units,int maxlen)
+{
+//	if (FieldIndex<0 || FieldIndex>=fieldcount)
+		return ft_nomorefields;
+//	strlcpy(FieldName,fieldnames[FieldIndex],maxlen-1);
+//	strlcpy(Units,fieldunits_and_multiplechoicestrings[FieldIndex],maxlen-1);
+//	return fieldtypes[FieldIndex];
+}
+
+int DLL_EXPORT __stdcall ContentGetValue(char* FileName,int FieldIndex,int UnitIndex,void* FieldValue,int maxlen,int flags)
+{
+	TagLib::FileRef f(FileName);
+
+	if (f.isNull() || !f.tag() )
+		return ft_fileerror;
+
+	TagLib::Tag *tag = f.tag();
+	//tag->
+
+	return ft_nosuchfield	;
+}
+
