e63a9e8e7896ca6ff3458538476eae90ca31f66e
murzz <murzz@a772d836-fa84-11dd-8259-6defae454439>
2008-03-01 20:46:43 +0000

    Initial import for WDXTagLib project. This is the content plugin for Total Commander. It uses TagLib as an engine and gives access to audio files tags.

---
+++ src/contentplug.h
@@ -0,0 +1,45 @@
+#define ft_nomorefields 0
+#define ft_numeric_32 1
+#define ft_numeric_64 2
+#define ft_numeric_floating 3
+#define ft_date 4
+#define ft_time 5
+#define ft_boolean 6
+#define ft_multiplechoice 7
+#define ft_string 8
+#define ft_fulltext 9
+
+// for ContentGetValue
+#define ft_delayed 0
+#define ft_nosuchfield -1
+#define ft_fileerror -2
+#define ft_fieldempty -3
+#define ft_ondemand -4
+
+#define CONTENT_DELAYIFSLOW 1
+
+typedef struct {
+    int size;
+    DWORD PluginInterfaceVersionLow;
+    DWORD PluginInterfaceVersionHi;
+    char DefaultIniName[MAX_PATH];
+} ContentDefaultParamStruct;
+
+typedef struct {
+	WORD wYear;
+	WORD wMonth;
+	WORD wDay;
+} *pdateformat;
+
+typedef struct {
+	WORD wHour;
+	WORD wMinute;
+	WORD wSecond;
+} *ptimeformat;
+/*
+int __stdcall ContentGetDetectString(char* DetectString,int maxlen);
+int __stdcall ContentGetSupportedField(int FieldIndex,char* FieldName,char* Units,char* MultipleChoiceStrings,int maxlen);
+int __stdcall ContentGetValue(char* FileName,int FieldIndex,int UnitIndex,void* FieldValue,int maxlen,int flags);
+void __stdcall ContentSetDefaultParams(ContentDefaultParamStruct* dps);
+void __stdcall ContentStopGetValue(char* FileName);
+*/
---
+++ src/main.h
@@ -0,0 +1,33 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <windows.h>
+#include "contentplug.h"
+
+/*  To use this exported function of dll, include this header
+ *  in your project.
+ */
+
+#ifdef BUILD_DLL
+    #define DLL_EXPORT __declspec(dllexport)
+#else
+    #define DLL_EXPORT __declspec(dllimport)
+#endif
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+//void DLL_EXPORT SomeFunction(const LPCSTR sometext);
+//int DLL_EXPORT ContentGetDetectString(char* DetectString,int maxlen);
+void DLL_EXPORT __stdcall ContentSetDefaultParams(ContentDefaultParamStruct* dps);
+int DLL_EXPORT __stdcall ContentGetSupportedField(int FieldIndex,char* FieldName,char* Units,int maxlen);
+int DLL_EXPORT __stdcall ContentGetValue(char* FileName,int FieldIndex,int UnitIndex,void* FieldValue,int maxlen,int flags);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __MAIN_H__
