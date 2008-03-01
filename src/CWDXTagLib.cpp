e9a720096fd537157e8e28c0323662b76d5e752f
murzz <murzz@a772d836-fa84-11dd-8259-6defae454439>
2008-03-01 22:27:01 +0000

    Added WDX base and taglib specific classes.

---
+++ src/CWDXTagLib.cpp
@@ -0,0 +1,19 @@
+#include "CWDXTagLib.h"
+#include <fileref.h>
+#include <tag.h>
+
+namespace WDXTagLib
+{
+
+CWDXTagLib::CWDXTagLib()
+{
+	//ctor
+}
+
+CWDXTagLib::~CWDXTagLib()
+{
+	//dtor
+}
+
+
+}
---
+++ src/CWDXTagLib.h
@@ -0,0 +1,19 @@
+#ifndef CTAGLIB_H
+#define CTAGLIB_H
+
+#include "CWDXBase.h"
+
+
+namespace WDXTagLib
+{
+	class CWDXTagLib : public CWDXBase
+	{
+		public:
+			CWDXTagLib();
+			virtual ~CWDXTagLib();
+
+		protected:
+		private:
+	};
+};
+#endif // CTAGLIB_H
