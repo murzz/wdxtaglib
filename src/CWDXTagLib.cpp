8b93e62500741e0f18557fc5c930cd9c5341563b
murzz <murzz@a772d836-fa84-11dd-8259-6defae454439>
2008-03-05 13:51:46 +0000

    GPL notice added in the beginning of all source files. Full text of the license is stored in license.txt.

--- src/CWDXTagLib.cpp
+++ src/CWDXTagLib.cpp
@@ -1,3 +1,20 @@
+// 	WDXTagLib is a content plugin for Total Commander that allows you to show audio
+// file tags in columns, edit tags and search in tags.
+// Copyright (C) 2008 Dmitrie Murzaikin (murzzz@gmail.com)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "CWDXTagLib.h"
 #include "CUtils.h"
 #include <fileref.h>
