b31bcb48d0e5fbe3b623798b4a479f08cdca4429
murzz <murzz@a772d836-fa84-11dd-8259-6defae454439>
2008-03-19 13:47:34 +0000

    Content Plugin API ContentPluginUnloading() function added.

--- trunk/src/main.h
+++ trunk/src/main.h
@@ -39,6 +39,7 @@ int DLL_EXPORT __stdcall ContentGetSupportedField(int FieldIndex,char* FieldName
 int DLL_EXPORT __stdcall ContentGetValue(char* FileName,int FieldIndex,int UnitIndex,void* FieldValue,int maxlen,int flags);
 int DLL_EXPORT __stdcall ContentGetSupportedFieldFlags(int FieldIndex);
 int DLL_EXPORT __stdcall ContentSetValue(char* FileName,int FieldIndex,int UnitIndex,int FieldType,void* FieldValue,int flags);
+void DLL_EXPORT __stdcall ContentPluginUnloading(void);
 
 #ifdef __cplusplus
 }
