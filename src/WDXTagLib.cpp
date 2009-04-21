86599d888f3d70618332ad58fc2c2ff6e5811490
murzzz <murzzz@a772d836-fa84-11dd-8259-6defae454439>
2009-04-21 19:23:18 +0000

    Refactoring: module CWDXTagLib renamed to WDXTagLib.

--- src/WDXTagLib.cpp
+++ src/WDXTagLib.cpp
@@ -16,7 +16,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-#include "CWDXTagLib.h"
+#include "WDXTagLib.h"
 #include "CUtils.h"
 #include <tag.h>
 
--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "main.h"
-#include "CWDXTagLib.h"
+#include "WDXTagLib.h"
 #include "CUtils.h"
 
 template <class T>
