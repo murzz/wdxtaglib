edfa62128b1e3668c45100ef6827a7038cfdba50
murzzz <murzzz@a772d836-fa84-11dd-8259-6defae454439>
2010-09-02 19:05:20 +0000

    Make it clear what kind of plugin is in use

--- trunk/src/Fields.h
+++ trunk/src/Fields.h
@@ -16,7 +16,7 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
-#include "PluginBase.h"
+#include "ContentPluginBase.h"
 #include <fileref.h>
 
 class PluginField : public WDX_API::FieldBase
