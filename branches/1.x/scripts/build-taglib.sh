f6f9945c91f42392f6aa8151bef0c896da4232cf
murzzz@gmail.com <murzzz@gmail.com@a772d836-fa84-11dd-8259-6defae454439>
2013-05-29 15:02:20 +0000

    branching development of 1.x

---
+++ branches/1.x/scripts/build-taglib.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+ver=1.8
+package=taglib-$ver
+archive=$package.tar.gz
+url=http://taglib.github.io/releases/$archive
+workdir=/tmp/$package
+#stagedir="$workdir/install"
+stagedir="$2"
+downloaded="$workdir/$archive"
+builddir="$workdir/build"
+#toolchainfile="/home/dm/src/wdxtaglib/latest-taglib/mk/toolchain-cross-mingw32-linux.cmake"
+toolchainfile=$1
+
+if test ! -f "$toolchainfile"; then
+    echo "toolchain file not exists: $toolchainfile"
+    exit 1
+fi
+
+if test ! -d "$stagedir"; then
+    echo "stagedir not exists: $stagedir"
+    exit 1
+fi
+
+
+# download
+wgetparams="$wgetparams --output-document=$downloaded"
+wgetparams="$wgetparams –quiet"
+wgetparams="$wgetparams $url"
+
+#rm -rf "$workdir" || exit
+mkdir -p "$workdir" || exit
+if test ! -f "$downloaded"; then
+	wget $wgetparams || exit
+fi
+
+
+# untar
+if test ! -f "$workdir/$package/CMakeLists.txt"; then
+	tar -xf "$downloaded" -C "$workdir" || exit
+fi
+
+
+# build
+cmakeparams="$cmakeparams -DCMAKE_TOOLCHAIN_FILE=$toolchainfile"
+cmakeparams="$cmakeparams -DCMAKE_BUILD_TYPE=Release"
+cmakeparams="$cmakeparams -DCMAKE_INSTALL_PREFIX=$stagedir"
+cmakeparams="$cmakeparams -DENABLE_STATIC=ON"
+cmakeparams="$cmakeparams -DENABLE_STATIC_RUNTIME=ON"
+cmakeparams="$cmakeparams -DVISIBILITY_HIDDEN=ON"
+#cmakeparams="$cmakeparams -DBUILD_TESTS=ON"
+#cmakeparams="$cmakeparams -DBUILD_EXAMPLES=ON"
+
+
+#rm -rf "$builddir" || exit
+mkdir -p "$builddir" || exit
+cd "$builddir" || exit
+if test ! -f "$builddir/CMakeCache.txt"; then
+	cmake $cmakeparams "$workdir/$package" || exit
+fi
+make -j install || exit
---
+++ branches/1.x/src/CWDXBase.cpp
@@ -0,0 +1,178 @@
+// 	WDXTagLib is a content plugin for Total Commander that allows you to show audio
+// file tags in columns, edit tags and search in tags.
+// Copyright (C) 2008 Dmitrie Murzaikin (murzzz@gmail.com)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "CWDXBase.h"
+#include "CUtils.h"
+#include <exception>
+using namespace std;
+
+namespace WDXTagLib
+{
+CWDXBase::CWDXBase()
+:	m_PluginInterfaceVerionHi(0),
+	m_PluginInterfaceVerionLow(0)
+{
+	//ctor
+}
+
+CWDXBase::~CWDXBase()
+{
+	//dtor
+}
+
+string_t CWDXBase::GetDetectString() const
+{
+	return OnGetDetectString();
+}
+
+string_t CWDXBase::OnGetDetectString() const
+{
+	return "";
+}
+
+void CWDXBase::SetIniName(const string_t& sIniName)
+{
+	if (sIniName == m_IniName)
+		return;
+	m_IniName = sIniName;
+}
+
+string_t CWDXBase::GetIniName() const
+{
+	return m_IniName;
+}
+
+void CWDXBase::SetPluginInterfaceVersion(const DWORD dwHi, const DWORD dwLow)
+{
+	m_PluginInterfaceVerionHi = dwHi;
+	m_PluginInterfaceVerionLow = dwLow;
+}
+
+int CWDXBase::GetSupportedField( const int iFieldIndex, char* pszFieldName, char* pszUnits, int iMaxLen)
+{
+	try
+	{
+		if ( iFieldIndex < 0 || iFieldIndex >= (int)m_Fields.size() )
+			return ft_nomorefields;
+
+		const CField& f = m_Fields[ iFieldIndex ];
+		CUtils::strlcpy( pszFieldName, f.m_Name.c_str(), iMaxLen - 1 );
+		CUtils::strlcpy( pszUnits, f.m_MultChoice.c_str(), iMaxLen - 1 );
+		return f.m_Type;
+	}
+	catch(...)
+	{
+		ExceptionHandler();
+		return ft_nomorefields;
+	}
+}
+
+int CWDXBase::GetValue(const char* pszFileName, const int iFieldIndex,
+						const int iUnitIndex, void* pFieldValue, const int iMaxLen, const int iFlags)
+{
+	try
+	{
+		if (iUnitIndex < 0)
+			CUtils::ShowError(CUtils::Int2Str(iUnitIndex));
+
+		if ( iFieldIndex < 0 || iFieldIndex >= (int)m_Fields.size() )
+			return ft_nosuchfield;
+
+		return OnGetValue(pszFileName, iFieldIndex, iUnitIndex, pFieldValue, iMaxLen, iFlags);
+	}
+	catch(...)
+	{
+		ExceptionHandler();
+		return ft_fileerror;
+	}
+}
+
+int CWDXBase::SetValue(const char* FileName, const int FieldIndex,
+									const int UnitIndex, const int FieldType, const void* FieldValue, const int flags)
+{
+	try
+	{
+		if ( !FileName || (-1 == FieldIndex) ) // this indicates end of changing attributes
+		{
+			OnEndOfSetValue();
+			return ft_setsuccess;
+		}
+
+		if ( FieldIndex < 0 || FieldIndex >= (int)m_Fields.size() )
+			return ft_nosuchfield;
+
+		return OnSetValue(FileName, FieldIndex, UnitIndex, FieldType, FieldValue, flags);
+	}
+	catch(...)
+	{
+		ExceptionHandler();
+		return ft_fileerror;
+	}
+}
+
+int CWDXBase::OnSetValue(const string_t& sFileName, const int iFieldIndex,
+													const int iUnitIndex, const int iFieldType, const void* pFieldValue, const int iFlags)
+{
+	return ft_nosuchfield;
+}
+
+int CWDXBase::GetSupportedFieldFlags(const int iFieldIndex)
+{
+	try
+	{
+		if (-1 == iFieldIndex) // we should return a combination of all supported flags here
+		{
+			int iTotalFlags = 0;
+			for (CMapOfFields::iterator iter = m_Fields.begin(); iter != m_Fields.end(); ++iter)
+				if ((*iter).second.m_Flag)
+					iTotalFlags |= (*iter).second.m_Flag;
+			return iTotalFlags;
+		}
+
+		if ( iFieldIndex < 0 || iFieldIndex >= (int)m_Fields.size() )
+			return ft_nomorefields;
+
+		return m_Fields[iFieldIndex].m_Flag;
+	}
+	catch(...)
+	{
+		ExceptionHandler();
+		return 0; // not sure what to return here
+	}
+}
+
+void CWDXBase::OnEndOfSetValue()
+{
+}
+
+void CWDXBase::ExceptionHandler() const
+{
+	try
+	{
+		throw;
+	}
+	catch(const std::exception& e)
+	{
+		CUtils::ShowError(e.what());
+	}
+	catch(...)
+	{
+		CUtils::ShowError(TEXT("Unknown exception"));
+	}
+}
+
+}
