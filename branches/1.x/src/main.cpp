8ab64ec2c4829cd9fff5591e501b719180188c42
murzzz@gmail.com <murzzz@gmail.com@a772d836-fa84-11dd-8259-6defae454439>
2013-06-03 09:55:50 +0000

    declarations of content API removed from main.h

--- branches/1.x/src/main.cpp
+++ branches/1.x/src/main.cpp
@@ -63,12 +63,12 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
     return TRUE; // succesful
 }
 
-void DLL_EXPORT __stdcall ContentGetDetectString(char* DetectString,int maxlen)
+extern "C" void DLL_EXPORT __stdcall ContentGetDetectString(char* DetectString,int maxlen)
 {
 	CUtils::strlcpy(DetectString, GPlugin::instance().GetDetectString().c_str(), maxlen);
 }
 
-void DLL_EXPORT __stdcall ContentSetDefaultParams(ContentDefaultParamStruct* dps)
+extern "C" void DLL_EXPORT __stdcall ContentSetDefaultParams(ContentDefaultParamStruct* dps)
 {
 	if ( sizeof(ContentDefaultParamStruct) != dps->size)
 		return;
@@ -77,28 +77,28 @@ void DLL_EXPORT __stdcall ContentSetDefaultParams(ContentDefaultParamStruct* dps
 	GPlugin::instance().SetPluginInterfaceVersion(dps->PluginInterfaceVersionHi, dps->PluginInterfaceVersionLow);
 }
 
-void DLL_EXPORT __stdcall ContentPluginUnloading(void)
+extern "C" void DLL_EXPORT __stdcall ContentPluginUnloading(void)
 {
 	// free plugin instance here
 }
 
-int DLL_EXPORT __stdcall ContentGetSupportedField(int FieldIndex,char* FieldName,char* Units,int maxlen)
+extern "C" int DLL_EXPORT __stdcall ContentGetSupportedField(int FieldIndex,char* FieldName,char* Units,int maxlen)
 {
 	return GPlugin::instance().GetSupportedField(FieldIndex, FieldName, Units, maxlen);
 }
 
-int DLL_EXPORT __stdcall ContentGetValue(char* FileName, int FieldIndex,
+extern "C" int DLL_EXPORT __stdcall ContentGetValue(char* FileName, int FieldIndex,
 																int UnitIndex, void* FieldValue, int maxlen, int flags)
 {
 	return GPlugin::instance().GetValue(FileName, FieldIndex, UnitIndex, FieldValue, maxlen, flags);
 }
 
-int DLL_EXPORT __stdcall ContentGetSupportedFieldFlags(int FieldIndex)
+extern "C" int DLL_EXPORT __stdcall ContentGetSupportedFieldFlags(int FieldIndex)
 {
 	return GPlugin::instance().GetSupportedFieldFlags(FieldIndex);
 }
 
-int DLL_EXPORT __stdcall ContentSetValue(char* FileName, int FieldIndex,
+extern "C" int DLL_EXPORT __stdcall ContentSetValue(char* FileName, int FieldIndex,
 										int UnitIndex, int FieldType, void* FieldValue, int flags)
 {
 	return GPlugin::instance().SetValue(FileName, FieldIndex, UnitIndex, FieldType, FieldValue, flags);
--- branches/1.x/src/main.h
+++ branches/1.x/src/main.h
@@ -17,23 +17,8 @@
 
 #pragma once
 
-#include <windows.h>
-#include "contentplug.h"
-
 #ifdef BUILD_DLL
     #define DLL_EXPORT __declspec(dllexport)
 #else
     #define DLL_EXPORT __declspec(dllimport)
 #endif
-
-
-extern "C"
-{
-void DLL_EXPORT __stdcall ContentGetDetectString(char* DetectString,int maxlen);
-void DLL_EXPORT __stdcall ContentSetDefaultParams(ContentDefaultParamStruct* dps);
-int DLL_EXPORT __stdcall ContentGetSupportedField(int FieldIndex,char* FieldName,char* Units,int maxlen);
-int DLL_EXPORT __stdcall ContentGetValue(char* FileName,int FieldIndex,int UnitIndex,void* FieldValue,int maxlen,int flags);
-int DLL_EXPORT __stdcall ContentGetSupportedFieldFlags(int FieldIndex);
-int DLL_EXPORT __stdcall ContentSetValue(char* FileName,int FieldIndex,int UnitIndex,int FieldType,void* FieldValue,int flags);
-void DLL_EXPORT __stdcall ContentPluginUnloading(void);
-}
