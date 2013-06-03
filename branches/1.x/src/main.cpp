b4abccec219f492b94a7345420bbd3f751256914
murzzz@gmail.com <murzzz@gmail.com@a772d836-fa84-11dd-8259-6defae454439>
2013-06-03 10:06:49 +0000

    use reference to static to shorten calls

--- branches/1.x/src/main.cpp
+++ branches/1.x/src/main.cpp
@@ -37,7 +37,7 @@ private:
     inline ~singleton() {}
 };
 
-typedef singleton<WDXTagLib::CWDXTagLib> GPlugin;
+WDXTagLib::CWDXTagLib& plugin = singleton<WDXTagLib::CWDXTagLib>::instance();
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
@@ -65,7 +65,7 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 
 extern "C" void DLL_EXPORT __stdcall ContentGetDetectString(char* DetectString,int maxlen)
 {
-	CUtils::strlcpy(DetectString, GPlugin::instance().GetDetectString().c_str(), maxlen);
+	CUtils::strlcpy(DetectString, plugin.GetDetectString().c_str(), maxlen);
 }
 
 extern "C" void DLL_EXPORT __stdcall ContentSetDefaultParams(ContentDefaultParamStruct* dps)
@@ -73,8 +73,8 @@ extern "C" void DLL_EXPORT __stdcall ContentSetDefaultParams(ContentDefaultParam
 	if ( sizeof(ContentDefaultParamStruct) != dps->size)
 		return;
 
-	GPlugin::instance().SetIniName(dps->DefaultIniName);
-	GPlugin::instance().SetPluginInterfaceVersion(dps->PluginInterfaceVersionHi, dps->PluginInterfaceVersionLow);
+	plugin.SetIniName(dps->DefaultIniName);
+	plugin.SetPluginInterfaceVersion(dps->PluginInterfaceVersionHi, dps->PluginInterfaceVersionLow);
 }
 
 extern "C" void DLL_EXPORT __stdcall ContentPluginUnloading(void)
@@ -84,22 +84,22 @@ extern "C" void DLL_EXPORT __stdcall ContentPluginUnloading(void)
 
 extern "C" int DLL_EXPORT __stdcall ContentGetSupportedField(int FieldIndex,char* FieldName,char* Units,int maxlen)
 {
-	return GPlugin::instance().GetSupportedField(FieldIndex, FieldName, Units, maxlen);
+	return plugin.GetSupportedField(FieldIndex, FieldName, Units, maxlen);
 }
 
 extern "C" int DLL_EXPORT __stdcall ContentGetValue(char* FileName, int FieldIndex,
 																int UnitIndex, void* FieldValue, int maxlen, int flags)
 {
-	return GPlugin::instance().GetValue(FileName, FieldIndex, UnitIndex, FieldValue, maxlen, flags);
+	return plugin.GetValue(FileName, FieldIndex, UnitIndex, FieldValue, maxlen, flags);
 }
 
 extern "C" int DLL_EXPORT __stdcall ContentGetSupportedFieldFlags(int FieldIndex)
 {
-	return GPlugin::instance().GetSupportedFieldFlags(FieldIndex);
+	return plugin.GetSupportedFieldFlags(FieldIndex);
 }
 
 extern "C" int DLL_EXPORT __stdcall ContentSetValue(char* FileName, int FieldIndex,
 										int UnitIndex, int FieldType, void* FieldValue, int flags)
 {
-	return GPlugin::instance().SetValue(FileName, FieldIndex, UnitIndex, FieldType, FieldValue, flags);
+	return plugin.SetValue(FileName, FieldIndex, UnitIndex, FieldType, FieldValue, flags);
 }
