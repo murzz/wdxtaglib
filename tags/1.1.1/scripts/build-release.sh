49c50dd6604ace2d04acfd42c647001b9561f1de
murzzz@gmail.com <murzzz@gmail.com@a772d836-fa84-11dd-8259-6defae454439>
2013-06-05 07:27:58 +0000

    tagging release v1.1.1

---
+++ tags/1.1.1/scripts/build-release.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+working_root="/tmp"
+wdxtaglibstagedir="/tmp/wdxtaglib-stage"
+wdxtaglibbuilddir="/tmp/wdxtaglib-build"
+package="wdx_WDXTagLib_1.1.1.zip"
+artifact="wdxtaglib.wdx"
+artifact64="$artifact"64
+
+rm -rf "$wdxtaglibstagedir" || exit
+rm -rf "$wdxtaglibbuilddir" || exit
+./build-all.sh "$wdxtaglibstagedir" "$wdxtaglibbuilddir" || exit
+
+# save package
+cp "$wdxtaglibbuilddir/$package" "$working_root" || exit
+
+rm -rf "$wdxtaglibstagedir" || exit
+rm -rf "$wdxtaglibbuilddir" || exit
+./build-all.sh "$wdxtaglibstagedir" "$wdxtaglibbuilddir" 64 || exit
+
+# add 64bit artifact to package
+mv "$wdxtaglibstagedir/$artifact" "$wdxtaglibstagedir/$artifact64" || exit
+cd "$wdxtaglibstagedir" || exit
+zip -g "$working_root/$package" "$artifact64" || exit
+zip -d "$working_root/$package" "libwdxtaglib.dll.a" || exit
---
+++ tags/1.1.1/src/plugin.h
@@ -0,0 +1,49 @@
+// WDXTagLib is a content plugin for Total Commander which uses TagLib library
+// for reading and editing the meta-data of several popular audio formats.
+// Copyright (C) 2008-2013 Dmitry Murzaikin (murzzz@gmail.com)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <map>
+#include <fileref.h>
+#include "base.h"
+
+namespace wdx
+{
+class plugin: public base
+{
+public:
+   plugin();
+   virtual ~plugin();
+
+private:
+   void OnInitFields();
+   int OnGetValue(const std::wstring& sFileName, const int FieldIndex,
+         const int UnitIndex, void* FieldValue, const int maxlen, const int flags);
+   int OnSetValue(const std::wstring& sFileName, const int FieldIndex,
+         const int UnitIndex, const int FieldType, const void* FieldValue, const int flags);
+
+   std::string OnGetDetectString() const;
+   void OnEndOfSetValue();
+
+   TagLib::FileRef& OpenFile(const std::wstring& sFileName);
+   std::string GetTagType(TagLib::File* pFile) const;
+
+   typedef std::map<std::wstring, TagLib::FileRef> files_t;
+
+   files_t Files2Write_;
+};
+}
