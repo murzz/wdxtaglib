57c36ae251c5502171f08dede69db3cd07ec3689
murzzz@gmail.com <murzzz@gmail.com@a772d836-fa84-11dd-8259-6defae454439>
2014-01-07 16:15:12 +0000

    updated build/release scripts

--- branches/1.x/scripts/build-release.sh
+++ branches/1.x/scripts/build-release.sh
@@ -1,25 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
-working_root="/tmp"
-wdxtaglibstagedir="/tmp/wdxtaglib-stage"
-wdxtaglibbuilddir="/tmp/wdxtaglib-build"
-artifact="wdxtaglib.wdx"
-artifact64="$artifact"64
-
-rm -rf "$wdxtaglibstagedir" || exit
-rm -rf "$wdxtaglibbuilddir" || exit
-./build-all.sh "$wdxtaglibstagedir" "$wdxtaglibbuilddir" || exit
-package=$(cat "$wdxtaglibbuilddir/package_name")
-
-# save package
-cp "$wdxtaglibbuilddir/$package" "$working_root" || exit
-
-rm -rf "$wdxtaglibstagedir" || exit
-rm -rf "$wdxtaglibbuilddir" || exit
-./build-all.sh "$wdxtaglibstagedir" "$wdxtaglibbuilddir" 64 || exit
-
-# add 64bit artifact to package
-mv "$wdxtaglibstagedir/$artifact" "$wdxtaglibstagedir/$artifact64" || exit
-cd "$wdxtaglibstagedir" || exit
-zip -g "$working_root/$package" "$artifact64" || exit
-zip -d "$working_root/$package" "libwdxtaglib.dll.a" || exit
+source _env.sh || echo "Run me from scripts directory" || exit
+_build_release $@ || exit
