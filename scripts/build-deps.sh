40ced621d7a8f17378df98a1809bf48ada8f624c
murzzz@gmail.com <murzzz@gmail.com@a772d836-fa84-11dd-8259-6defae454439>
2014-01-09 11:49:59 +0000

    fix broken and install suggests

--- scripts/build-deps.sh
+++ scripts/build-deps.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 sudo apt-get autoclean && \
 sudo apt-get update && \
-sudo apt-get --force-yes install \
-   wget \
-   zip \
-   cmake \
-   build-essential \
-   mingw-w64 \
-   g++-mingw-w64
+sudo apt-get \
+   --force-yes \
+   --install-suggests \
+   --fix-broken \
+   install \
+      wget \
+      zip \
+      cmake \
+      build-essential \
+      mingw-w64 \
+      g++-mingw-w64
