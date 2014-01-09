342c6342cd6e25878668467d7f8302f401c8f554
murzzz@gmail.com <murzzz@gmail.com@a772d836-fa84-11dd-8259-6defae454439>
2014-01-09 13:37:39 +0000

    added build dep libz-dev

--- scripts/build-deps.sh
+++ scripts/build-deps.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get install wget zip cmake && \
 sudo apt-get --assume-yes remove mingw32 && \
-sudo apt-get --assume-yes install build-essential mingw-w64 g++-mingw-w64
+sudo apt-get --assume-yes install build-essential mingw-w64 g++-mingw-w64 libz-dev
