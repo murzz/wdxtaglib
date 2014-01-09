82ee3986fc8fa608136c0df5e431ebcb645c9347
murzzz@gmail.com <murzzz@gmail.com@a772d836-fa84-11dd-8259-6defae454439>
2014-01-09 12:17:32 +0000

    travis ci

--- trunk/scripts/build-deps.sh
+++ trunk/scripts/build-deps.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-sudo apt-get autoremove && \
-sudo apt-get clean && \
-sudo apt-get install --fix-broken && \
-sudo apt-get update && \
-sudo apt-get \
-   --assume-yes \
-   --install-suggests \
-   install \
-      wget \
-      zip \
-      cmake \
-      build-essential \
-      mingw-w64 \
-      g++-mingw-w64
+uname -a
+#sudo apt-get autoremove && \
+#sudo apt-get clean && \
+#sudo apt-get install --fix-broken && \
+#sudo apt-get update && \
+#sudo apt-get \
+   #   --assume-yes \
+   #--install-suggests \
+   #install \
+      #   wget \
+      #zip \
+      #cmake \
+      #build-essential \
+      #mingw-w64 \
+      #g++-mingw-w64
