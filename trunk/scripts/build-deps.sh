fc863ba36c0c9dc1b823d4c3e0f1341d319d769b
murzzz@gmail.com <murzzz@gmail.com@a772d836-fa84-11dd-8259-6defae454439>
2014-01-09 11:44:31 +0000

    autoclean added

--- trunk/scripts/build-deps.sh
+++ trunk/scripts/build-deps.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo apt-get update && sudo apt-get -y install wget zip cmake build-essential mingw-w64 g++-mingw-w64
+sudo apt-get autoclean && sudo apt-get update && sudo apt-get --force-yes install wget zip cmake build-essential mingw-w64 g++-mingw-w64
