129525f28b21bc6f68434649bbefd8c82639ac61
murzzz <murzzz@gmail.com>
2015-11-23 14:49:46 +0200

    ninja as build backend

--- scripts/build-deps.sh
+++ scripts/build-deps.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sudo apt-get install wget zip cmake && \
+sudo apt-get install wget zip cmake ninja-build && \
 sudo apt-get --assume-yes remove mingw32 && \
 sudo apt-get --assume-yes install build-essential mingw-w64 g++-mingw-w64 libz-dev
