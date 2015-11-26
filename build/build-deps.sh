e1e1050b0cfd23e461db520f9a612bc8e141c68a
dm <murzzz@gmail.com>
2015-11-26 23:38:38 +0200

    travis dist-upgrade

--- build/build-deps.sh
+++ build/build-deps.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sudo apt-get update && \
+sudo apt-get dist-upgrade && \
 sudo apt-get install wget zip cmake ninja-build && \
 sudo apt-get --assume-yes remove mingw32 && \
 sudo apt-get --assume-yes install build-essential mingw-w64 g++-mingw-w64 libz-dev
