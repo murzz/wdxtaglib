d5b11123c0d11fd3f66456e0826f5d3ae8f87a3a
dm <murzzz@gmail.com>
2015-11-27 00:15:05 +0200

    no distupgrade, it stucks waiting for user input

--- build/build-deps.sh
+++ build/build-deps.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-sudo apt-get --assume-yes update && \
-sudo apt-get --assume-yes dist-upgrade && \
-sudo apt-get install wget zip cmake ninja-build && \
-sudo apt-get --assume-yes remove mingw32 && \
-sudo apt-get --assume-yes install build-essential mingw-w64 g++-mingw-w64 libz-dev
+sudo apt-get update && \
+sudo apt-get -y remove mingw32 && \
+sudo apt-get -y install wget zip build-essential mingw-w64 g++-mingw-w64 libz-dev
